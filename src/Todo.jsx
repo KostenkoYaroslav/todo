eefcfe6b0e953df52d6f2db2a1b4e6bc66b11c85
KostenkoYaroslav <yaroslavkostenko34@gmail.com>
2024-03-17 16:13:37 +0200

    first commit

---
+++ src/Todo.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+export default function Todo() {
+  const [task, setTask] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  function addTask() {}
+
+  function deleteTask() {}
+
+  function editTask() {}
+
+  return <></>;
+}
