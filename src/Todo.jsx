dcfdfe768273ddb7070e353ee683603d1846063f
KostenkoYaroslav <yaroslavkostenko34@gmail.com>
2024-03-20 18:16:45 +0200

    add deleteTask, editTask

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,39 +1,74 @@
 import React, { useState } from "react";
+import { FaTrash } from "react-icons/fa";
+import { TbEdit } from "react-icons/tb";
 
 export default function Todo() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
 
-  function addTask() {}
+  function handleInputChange(event) {
+    setNewTask(event.target.value);
+  }
 
-  function deleteTask() {}
+  function addTask() {
+    if (newTask.trim() !== "") {
+      setTasks([...tasks, newTask]);
+      setNewTask("");
+    }
+  }
 
-  function editTask() {}
+  function deleteTask(index) {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  }
+
+  function editTask(index, updatedTask) {
+    const updatedTasks = [...tasks];
+    updatedTasks[index] = updatedTask;
+    setTasks(updatedTasks);
+  }
 
   return (
     <main className="flex flex-col items-center justify-center">
-      <h1 className="pt-10 text-xl uppercase font-bold">
-        Xero<span className="text-orange-500">Todo</span>
-      </h1>
-      <div className="flex items-center">
-        <input
-          type="text"
-          placeholder="write your next task"
-          className="bg-[#1E1E1E] outline-none px-4 py-2 mr-3 rounded-xl placeholder:text-[#E8D7B9] placeholder:opacity-50"
-        />
-        <button className="bg-orange-500 rounded-full px-4 py-1 text-3xl text-black font-bold">
-          +
-        </button>
+      <div>
+        <h1 className="pt-10 text-xl uppercase font-bold">
+          Xero<span className="text-orange-500">Todo</span>
+        </h1>
+        <div className="flex items-center">
+          <input
+            type="text"
+            placeholder="write your next task"
+            value={newTask}
+            onChange={handleInputChange}
+            className="bg-[#1E1E1E] outline-none px-4 py-2 mr-3 rounded-xl placeholder:text-[#E8D7B9] placeholder:opacity-50"
+          />
+          <button
+            className="bg-orange-500 rounded-full px-4 py-1 text-3xl text-black font-bold"
+            onClick={addTask}
+          >
+            +
+          </button>
+        </div>
+        <ul>
+          {tasks.map((task, index) => (
+            <li
+              key={index}
+              className="bg-[#1E1E1E] w-full p-4 rounded-xl border border-orange-200 flex  "
+            >
+              <span className="flex-1">{task}</span>
+              <button
+                className="mr-1"
+                onClick={() => editTask(index, prompt("Enter new task:", task))}
+              >
+                <TbEdit size={25} />
+              </button>
+              <button onClick={() => deleteTask(index)}>
+                <FaTrash size={20} />
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
-      <ul>
-        {tasks.map((task, index) => (
-          <li key={index}>
-            <span>{task}</span>
-            <button>Edit</button>
-            <button>Delete</button>
-          </li>
-        ))}
-      </ul>
     </main>
   );
 }
