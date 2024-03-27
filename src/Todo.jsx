e0041948f67efc7395b2907cf1055bed64a3b3c3
KostenkoYaroslav <yaroslavkostenko34@gmail.com>
2024-03-27 19:49:13 +0200

    delete functions

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -6,27 +6,13 @@ export default function Todo() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
 
-  function handleInputChange(event) {
-    setNewTask(event.target.value);
-  }
+  function handleInputChange() {}
 
-  function addTask() {
-    if (newTask.trim() !== "") {
-      setTasks([...tasks, newTask]);
-      setNewTask("");
-    }
-  }
+  function addTask() {}
 
-  function deleteTask(index) {
-    const updatedTasks = tasks.filter((_, i) => i !== index);
-    setTasks(updatedTasks);
-  }
+  function deleteTask() {}
 
-  function editTask(index, updatedTask) {
-    const updatedTasks = [...tasks];
-    updatedTasks[index] = updatedTask;
-    setTasks(updatedTasks);
-  }
+  function editTask() {}
 
   return (
     <main className="flex flex-col items-center justify-center">
@@ -38,35 +24,22 @@ export default function Todo() {
           <input
             type="text"
             placeholder="write your next task"
-            value={newTask}
-            onChange={handleInputChange}
             className="bg-[#1E1E1E] outline-none px-4 py-2 mr-3 rounded-xl placeholder:text-[#E8D7B9] placeholder:opacity-50"
           />
-          <button
-            className="bg-orange-500 rounded-full px-4 py-1 text-3xl text-black font-bold"
-            onClick={addTask}
-          >
+          <button className="bg-orange-500 rounded-full px-4 py-1 text-3xl text-black font-bold">
             +
           </button>
         </div>
         <ul>
-          {tasks.map((task, index) => (
-            <li
-              key={index}
-              className="bg-[#1E1E1E] w-full p-4 rounded-xl border border-orange-200 flex  "
-            >
-              <span className="flex-1">{task}</span>
-              <button
-                className="mr-1"
-                onClick={() => editTask(index, prompt("Enter new task:", task))}
-              >
-                <TbEdit size={25} />
-              </button>
-              <button onClick={() => deleteTask(index)}>
-                <FaTrash size={20} />
-              </button>
-            </li>
-          ))}
+          <li className="bg-[#1E1E1E] w-full p-4 rounded-xl border border-orange-200 flex  ">
+            <span className="flex-1"></span>
+            <button className="mr-1">
+              <TbEdit size={25} />
+            </button>
+            <button>
+              <FaTrash size={20} />
+            </button>
+          </li>
         </ul>
       </div>
     </main>
