d794fdc564b7dca94dc07309a6fc968b00152757
KostenkoYaroslav <yaroslavkostenko34@gmail.com>
2024-03-29 21:01:09 +0200

    addTask and deleteTask update

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -6,11 +6,17 @@ export default function Todo() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
 
-  function handleInputChange() {}
+  function addTask(e) {
+    e.preventDefault();
+    if (newTask.trim() !== "") {
+      setTasks([...tasks, { content: newTask }]);
+      setNewTask("");
+    }
+  }
 
-  function addTask() {}
-
-  function deleteTask() {}
+  function deleteTask() {
+    setTasks(tasks.filter((a) => a.id !== tasks.id));
+  }
 
   function editTask() {}
 
@@ -20,26 +26,34 @@ export default function Todo() {
         <h1 className="pt-10 text-xl uppercase font-bold">
           Xero<span className="text-orange-500">Todo</span>
         </h1>
-        <div className="flex items-center">
+        <form className="flex items-center" onSubmit={addTask}>
           <input
             type="text"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
             placeholder="write your next task"
             className="bg-[#1E1E1E] outline-none px-4 py-2 mr-3 rounded-xl placeholder:text-[#E8D7B9] placeholder:opacity-50"
           />
           <button className="bg-orange-500 rounded-full px-4 py-1 text-3xl text-black font-bold">
             +
           </button>
-        </div>
+        </form>
         <ul>
-          <li className="bg-[#1E1E1E] w-full p-4 rounded-xl border border-orange-200 flex  ">
-            <span className="flex-1"></span>
-            <button className="mr-1">
-              <TbEdit size={25} />
-            </button>
-            <button>
-              <FaTrash size={20} />
-            </button>
-          </li>
+          {tasks.map((task) => (
+            <li
+              key={task}
+              className="bg-[#1E1E1E] w-full p-4 rounded-xl border border-orange-200 flex  "
+            >
+              <button className=" rounded-full border border-orange-500 w-6 h-6"></button>
+              <span className="flex-1">{task.content}</span>
+              <button className="mr-1">
+                <TbEdit size={25} />
+              </button>
+              <button onClick={deleteTask}>
+                <FaTrash size={20} />
+              </button>
+            </li>
+          ))}
         </ul>
       </div>
     </main>
