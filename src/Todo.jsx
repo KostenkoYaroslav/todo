c0780e6edb3705b9fa491db9320199d8804cfad9
KostenkoYaroslav <yaroslavkostenko34@gmail.com>
2024-04-09 16:46:24 +0300

    responsiv update

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -81,9 +81,9 @@ export default function Todo() {
             value={newTask}
             onChange={(e) => setNewTask(e.target.value)}
             placeholder="write your next task"
-            className="bg-[#1E1E1E] outline-none pl-4 pr-16 py-2 mr-3 rounded-2xl placeholder:text-[#E8D7B9] placeholder:opacity-50"
+            className="bg-[#1E1E1E] outline-none pl-4 pr-16 py-2 mr-3 rounded-2xl placeholder:text-[#E8D7B9] placeholder:opacity-50 max-sm:pr-5"
           />
-          <button className="bg-orange-500 rounded-full px-5 py-2 text-3xl text-black font-bold">
+          <button className="bg-orange-500 rounded-full px-4 py-1 text-3xl text-black font-bold">
             +
           </button>
         </form>
